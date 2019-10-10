8a2fdb53bed459b6cfe5fa5f8795d0008071c5d9
Muhammed Shafi <shafimsp@gmail.com>
2019-10-10 21:03:03 +0530

    ios fix

--- ios/RNPayTm.h
+++ ios/RNPayTm.h
@@ -1,7 +1,7 @@
 
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
-#import "sdk/headers/PaymentsSDK.h"
+#import "PaymentsSDK.h"
 
 @interface RNPayTm : RCTEventEmitter <RCTBridgeModule>
 
