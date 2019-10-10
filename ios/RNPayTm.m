38f31ae6a3d315475092937e7fcd64d526d65a64
Muhammed Shafi <shafimsp@gmail.com>
2019-10-10 21:13:01 +0530

    ios fix

--- ios/RNPayTm.m
+++ ios/RNPayTm.m
@@ -6,48 +6,48 @@
 
 
 PGTransactionViewController* txnController;
-
-RCT_EXPORT_MODULE()
-
-RCT_EXPORT_METHOD(startPayment: (NSDictionary *)details)
-{
-    NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    for (NSHTTPCookie *cookie in [storage cookies]) {
-      [storage deleteCookie:cookie];
-    }
-    [[NSUserDefaults standardUserDefaults] synchronize];
-
-    PGMerchantConfiguration *mc = [PGMerchantConfiguration defaultConfiguration];
-    NSMutableDictionary *orderDict = [NSMutableDictionary new];
-
-    orderDict[@"MID"] = details[@"mid"];
-    orderDict[@"CHANNEL_ID"] = details[@"channel"];
-    orderDict[@"INDUSTRY_TYPE_ID"] = details[@"industryType"];
-    orderDict[@"WEBSITE"] = details[@"website"];
-    orderDict[@"TXN_AMOUNT"] = details[@"amount"];
-    orderDict[@"ORDER_ID"] = details[@"orderId"];
-    orderDict[@"EMAIL"] = details[@"email"];
-    orderDict[@"MOBILE_NO"] = details[@"phone"];
-    orderDict[@"CUST_ID"] = details[@"custId"];
-    orderDict[@"CHECKSUMHASH"] = details[@"checksumhash"];
-    orderDict[@"CALLBACK_URL"] = details[@"callback"];
-
-    PGOrder *order = [PGOrder orderWithParams:orderDict];
-
-    //PGTransactionViewController and set the serverType to eServerTypeProduction
-    txnController = [[PGTransactionViewController alloc] initTransactionForOrder:order];
-    txnController.serverType = eServerTypeStaging;
-    txnController.merchant = mc;
-    txnController.delegate = self;
-    txnController.title = @"Paytm payment";
-
-    UIViewController *rootVC = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [rootVC presentViewController:txnController animated:YES completion:nil];
-      });
-
-}
+//
+//RCT_EXPORT_MODULE()
+//
+//RCT_EXPORT_METHOD(startPayment: (NSDictionary *)details)
+//{
+//    NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+//    for (NSHTTPCookie *cookie in [storage cookies]) {
+//      [storage deleteCookie:cookie];
+//    }
+//    [[NSUserDefaults standardUserDefaults] synchronize];
+//
+//    PGMerchantConfiguration *mc = [PGMerchantConfiguration defaultConfiguration];
+//    NSMutableDictionary *orderDict = [NSMutableDictionary new];
+//
+//    orderDict[@"MID"] = details[@"mid"];
+//    orderDict[@"CHANNEL_ID"] = details[@"channel"];
+//    orderDict[@"INDUSTRY_TYPE_ID"] = details[@"industryType"];
+//    orderDict[@"WEBSITE"] = details[@"website"];
+//    orderDict[@"TXN_AMOUNT"] = details[@"amount"];
+//    orderDict[@"ORDER_ID"] = details[@"orderId"];
+//    orderDict[@"EMAIL"] = details[@"email"];
+//    orderDict[@"MOBILE_NO"] = details[@"phone"];
+//    orderDict[@"CUST_ID"] = details[@"custId"];
+//    orderDict[@"CHECKSUMHASH"] = details[@"checksumhash"];
+//    orderDict[@"CALLBACK_URL"] = details[@"callback"];
+//
+//    PGOrder *order = [PGOrder orderWithParams:orderDict];
+//
+//    //PGTransactionViewController and set the serverType to eServerTypeProduction
+//    txnController = [[PGTransactionViewController alloc] initTransactionForOrder:order];
+//    txnController.serverType = eServerTypeStaging;
+//    txnController.merchant = mc;
+//    txnController.delegate = self;
+//    txnController.title = @"Paytm payment";
+//
+//    UIViewController *rootVC = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+//
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//        [rootVC presentViewController:txnController animated:YES completion:nil];
+//      });
+//
+//}
 
 - (NSArray<NSString *> *)supportedEvents
 {
